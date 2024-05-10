02d7aef4bcf8470f966bf9e94a46f24a1f87821c
foryer9999 <foryerlucas@gmail.com>
2024-05-10 02:40:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,8 @@ const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
 
-    // Número de teléfono de WhatsApp (incluyendo el código de país)
-    const phoneNumber = '59160499541'
-    // Reemplaza con el número de WhatsApp deseado
-
     // Mensaje predeterminado
-    const message = encodeURIComponent('¡Sí, quiero ser tu novia!'); // Mensaje codificado para URL
-
-    // Crear la URL de WhatsApp con el número y el mensaje
-    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${message}`;
+    const message = Ya somos novios 
 
     // Abrir la URL en una nueva pestaña o ventana
     window.open(whatsappUrl, '_blank');
